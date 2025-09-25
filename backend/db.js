8c404b3251e6628ae28209607978ad90b0755fda
rahullade <rahullade935@gmail.com>
2025-09-25 05:20:50 +0000

    added account schema

--- backend/db.js
+++ backend/db.js
@@ -33,10 +33,28 @@ const userSchema = new mongoose.schema({
     }
 });
 
+// schema for account
+const accountSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    balance:{
+        type:Number,
+        required:true
+    }
+});
+
 // create a model from the schema 
-const User = mongoose.model('User,userSchema');
+const User = mongoose.model('User',userSchema);
+
+
+// account model for the schema
+const Account = mongoose.model('Account',accountSchema);
+
 
 // module exporting
 module.exports={
-    User
+    User,Account
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -111,15 +111,10 @@ router.get("/bulk",async(req,res)=>{
     const filter = req.query.filter ||"";
 
     const users = await User.find({
-        $or:[{
-            firstName:{
-                "$regex":filter
-            }
-        },{
-            lastName:{
-                "$regex":filter
-            }
-        }]
+         $or: [
+    { firstName: { "$regex": filter } },
+    { lastName:  { "$regex": filter } }
+  ]
     })
     res.json({
         user:users.map(user=>({
